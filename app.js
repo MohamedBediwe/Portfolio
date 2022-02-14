a079d460c762b2f4b560969c155db88e2b8f2c17
MohamedBediwe <mohamed.bediwe@gmail.com>
2022-02-14 09:54:56 +0200

    Applied changes on the design

--- app.js
+++ app.js
@@ -1,6 +1,27 @@
 const bars = document.querySelector('.bars');
 const nav = document.querySelector('nav');
-
+const header = document.querySelector('header')
+const scrollToTop = document.querySelector('.scroll-to-top')
 bars.addEventListener('click', function () {
     nav.classList.toggle('active');
 })
+
+window.onscroll = function () {
+    if (window.scrollY >= 600) {
+        scrollToTop.style.display = "block";
+    } else {
+        scrollToTop.style.display = "none";
+    }
+    if (window.scrollY >= 200) {
+        header.classList.add('scrolled');
+    } else {
+        header.classList.remove('scrolled');
+    }
+}
+scrollToTop.onclick = function () {
+    window.scrollTo({
+        left: 0,
+        top: 0,
+        behavior: "smooth"
+    })
+}
